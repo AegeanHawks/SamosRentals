a1a41b566b54e7228126170fdd18fc3dbc345e5b
darknight <icsd11175@icsd.aegean.gr>
2015-05-03 22:36:42 +0300

    Sql tables update

--- samosrentals.sql
+++ samosrentals.sql
@@ -1,99 +1,127 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 02, 2015 at 12:26 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema samosrentals
+-- -----------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- -----------------------------------------------------
+-- Schema samosrentals
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `samosrentals` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `samosrentals` ;
 
---
--- Database: `samosrentals`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `auction`
---
-
-CREATE TABLE IF NOT EXISTS `auction` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `Description` varchar(255) NOT NULL,
-  `Status` int(11) NOT NULL,
-  `Bid_Price` int(11) NOT NULL,
-  `End_Price` int(11) NOT NULL,
-  `Hotel` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`),
-  KEY `Hotel` (`Hotel`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hotel`
---
-
-CREATE TABLE IF NOT EXISTS `hotel` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  `Tel` varchar(255) NOT NULL,
-  `Description` varchar(255) NOT NULL,
-  `Coordinates` varchar(255) NOT NULL,
-  `Grade` int(11) NOT NULL,
-  `Manager` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`),
-  KEY `Manager` (`Manager`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `Username` varchar(255) NOT NULL DEFAULT '',
-  `Password` varchar(30) NOT NULL,
-  `LastName` varchar(30) NOT NULL,
-  `FirstName` varchar(30) NOT NULL,
-  `Sex` varchar(20) NOT NULL,
-  `Mail` varchar(255) NOT NULL,
-  `Birthday` date DEFAULT NULL,
-  `Role` int(11) NOT NULL,
+-- -----------------------------------------------------
+-- Table `samosrentals`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `samosrentals`.`User` (
+  `Username` VARCHAR(60) NOT NULL,
+  `Password` VARCHAR(30) NOT NULL,
+  `LastName` VARCHAR(30) NOT NULL,
+  `FirstName` VARCHAR(30) NOT NULL,
+  `Sex` VARCHAR(10) NOT NULL,
+  `Mail` VARCHAR(40) NULL,
+  `Birthday` VARCHAR(20) NULL,
+  `Role` INT NOT NULL,
+  `Image` TEXT NULL,
+  `Tel` VARCHAR(20) NULL,
   PRIMARY KEY (`Username`),
-  UNIQUE KEY `Mail` (`Mail`),
-  UNIQUE KEY `Username` (`Username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  UNIQUE INDEX `Mail_UNIQUE` (`Mail` ASC))
+ENGINE = InnoDB;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `auction`
---
-ALTER TABLE `auction`
-  ADD CONSTRAINT `auction_ibfk_1` FOREIGN KEY (`Hotel`) REFERENCES `hotel` (`ID`);
+-- -----------------------------------------------------
+-- Table `samosrentals`.`Hotel`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `samosrentals`.`Hotel` (
+  `ID` INT NOT NULL,
+  `Name` VARCHAR(100) NULL,
+  `Tel` VARCHAR(40) NULL,
+  `Description` VARCHAR(500) NULL,
+  `Coordinates` VARCHAR(255) NULL,
+  `Grade` INT NULL,
+  `Manager` VARCHAR(60) NULL,
+  `Conforts` VARCHAR(200) NULL,
+  `Images` TEXT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `FK_Manager_idx` (`Manager` ASC),
+  CONSTRAINT `FK_Manager`
+    FOREIGN KEY (`Manager`)
+    REFERENCES `samosrentals`.`User` (`Username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Constraints for table `hotel`
---
-ALTER TABLE `hotel`
-  ADD CONSTRAINT `hotel_ibfk_1` FOREIGN KEY (`Manager`) REFERENCES `user` (`Username`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- -----------------------------------------------------
+-- Table `samosrentals`.`Auction`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `samosrentals`.`Auction` (
+  `ID` INT NOT NULL,
+  `Name` VARCHAR(100) NULL,
+  `Description` VARCHAR(500) NULL,
+  `Closed` TINYINT(1) NULL,
+  `Bid_Price` INT NULL,
+  `End_Price` INT NULL,
+  `idHotel` INT NULL,
+  `PeopleCount` INT NULL,
+  `End_Date` DATE NULL,
+  `Images` TEXT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `FK_idHotel_idx` (`idHotel` ASC),
+  CONSTRAINT `FK_idHotel`
+    FOREIGN KEY (`idHotel`)
+    REFERENCES `samosrentals`.`Hotel` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `samosrentals`.`Bid`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `samosrentals`.`Bid` (
+  `Username` VARCHAR(60) NOT NULL,
+  `idAuction` INT NOT NULL,
+  `BidMoney` DECIMAL(2) NULL,
+  `Won` TINYINT(1) NULL,
+  PRIMARY KEY (`Username`, `idAuction`),
+  INDEX `FK_AuctionId_idx` (`idAuction` ASC),
+  CONSTRAINT `FK_Username`
+    FOREIGN KEY (`Username`)
+    REFERENCES `samosrentals`.`User` (`Username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_AuctionId`
+    FOREIGN KEY (`idAuction`)
+    REFERENCES `samosrentals`.`Auction` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `samosrentals` ;
+
+-- -----------------------------------------------------
+-- procedure closeAuctions
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `samosrentals`$$
+CREATE PROCEDURE `closeAuctions` ()
+BEGIN
+SELECT @TIMESTAMP_NOW := NOW();
+START TRANSACTION;
+SELECT *  FROM Auction WHERE End_Date < @TIMESTAMP_NOW AND Closed = '0' FOR UPDATE;
+UPDATE Auction SET Closed = '1' WHERE End_Date < @TIMESTAMP_NOW;
+COMMIT;
+END
+$$
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
