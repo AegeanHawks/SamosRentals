0c3a1a6754504d45f678c5f75371bdbb29def20c
Nikolaos Bousios <icsd10124@icsd.aegean.gr>
2015-06-01 06:15:54 +0300

    github support fixed

--- githubupdate.php
+++ githubupdate.php
@@ -1,1 +1,6 @@
-<?php `git pull`; ?>
+<?php
+define('PRIVATE_KEY', '#github#samosrentals#project#');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_REQUEST['thing'] === PRIVATE_KEY) {
+    echo shell_exec("git pull");
+}
