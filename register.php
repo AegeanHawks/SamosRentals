3a99f6b451b401e17696ce427b9a075995bca271
Armageddonas <icsd11175@icsd.aegean.gr>
2017-12-26 04:08:00 +0200

    Datepicker fix

--- register.php
+++ register.php
@@ -326,7 +326,16 @@ if ($registered){
     </div>
     <!-- Registration Form -->
 </div>
-
+<script>
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 60, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+</script>
 </body>
 </html>
 <?php
