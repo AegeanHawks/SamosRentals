aae2ac30dae21ec0b7657f1d3e9f12dae405458b
Nikolas Bousios (Rambou) <nickos.bou@gmail.com>
2019-08-05 02:58:10 +0300

    path fixing

--- admin/configuration.php
+++ admin/configuration.php
@@ -1,8 +1,8 @@
 <?php
 
 // load enviromental variables
-require __DIR__.'/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::create(__DIR__);
+require __DIR__.'/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::create(__DIR__."/../");
 $dotenv->load();
 $dotenv->required(['DB_SERVER_NAME', 'DB_USERNAME', 'DB_PASSWORD', 'DB_NAME'])->notEmpty();
 
